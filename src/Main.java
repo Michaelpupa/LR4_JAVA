0240d06e663e0d4854b1fa7ae7b7b6ad0b8d0eea
misha <misha.sokolenko2004@gmail.com>
2022-10-22 12:51:11 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        System.out.println(Character.isAlphabetic(';'));
+
+        //task 2
+        System.out.println("task2:  " + "\u2639" + " " +"\u263B" + " " + "\u263A" );
+
+    }
+}
